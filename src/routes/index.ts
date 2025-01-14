5b352d16836f890f9bb8f30919139ccc5228a4f2
Arash Nozarinejad <nozarinejad.arash@gmail.com>
2025-01-14 13:18:27 +0400

    router index file created along with /heath, /version, / routes, as well as an error for unknown routes

---
+++ src/routes/index.ts
@@ -0,0 +1,32 @@
+import { Router } from "express";
+import { AppError } from "src/middlewares/error/errorHandler";
+
+const router = Router();
+
+router.get('/health', (_, res) => {
+    res.status(200).json({
+        status: 'success',
+        message: 'searver is healthy',
+        timestamp: new Date().toString(),
+    });
+});
+
+router.get('/version', (_, res) => {
+    res.status(200).json({
+        version: '1.0.0',
+        environment: process.env.NODE_ENV,
+    });
+});
+
+router.get('/', (_, res) => {
+    res.status(200).json({
+        message: 'Welcome to the RBAC API',
+        documentation: '/api/docs'
+    });
+});
+
+router.all('*', (req, _, next) => {
+    next(new AppError(404, `Cannot find ${req.originalUrl} on this server!`));
+});
+
+export default router;
